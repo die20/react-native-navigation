c48672820b04ec1548efb618236e775d6e6b186e
Sal Principato <sal.principato@gmail.com>
2019-06-26 15:52:59 -0400

    Update SideMenuController.java

--- lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/sidemenu/SideMenuController.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/sidemenu/SideMenuController.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.res.Resources;
 import androidx.annotation.NonNull;
 import androidx.drawerlayout.widget.DrawerLayout;
-import android.support.v4.widget.DrawerLayout.LayoutParams;
+import androidx.drawerlayout.widget.DrawerLayout.LayoutParams;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
